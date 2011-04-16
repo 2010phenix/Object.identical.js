4107b81872a369366ba8174c3183ad40827cfa38
Chris O <chris@prettycode.org>
2011-04-16 04:05:05 -0700

    Bug fix--checking for  values before trying to sort them

--- Object.identical.js
+++ Object.identical.js
@@ -1,26 +1,28 @@
 /*
-    Original script title: Object.identical.js, v1.1
+    Original script title/version: Object.identical.js/1.11
     Copyright (c) 2011, Chris O'Brien, prettycode.org
     http://github.com/prettycode/Object.identical.js
 
-    LICENSE: Permission is hereby granted for unrestricted use,
-    modification, and redistribution of this script, ONLY under
-    the condition that this code comment is kept wholly complete,
-    appearing above the script's code body--in all original or
-    modified implementations of this script, except those that
-    are minified.
+    LICENSE: Permission is hereby granted for unrestricted use, modification, and redistribution of this script, ONLY
+    under the condition that this code comment is kept wholly complete, appearing above the script's code body--in all
+    original or modified implementations of this script, except those that are minified.
 */
 
 Object.identical = function (a, b, sortArrays) {
   
-    // requires ECMAScript 5 Array.isArray(), Object.keys(), Array.prototype.forEach(), and JSON.stringify()
+    /* Requires ECMAScript 5 functions:
+           - Array.isArray()
+           - Object.keys()
+           - Array.prototype.forEach()
+           - JSON.stringify()
+    */
   
     function sort(o) {
         
         if (sortArrays === true && Array.isArray(o)) {
             return o.sort();
         }
-        else if (typeof o !== "object") {
+        else if (typeof o !== "object" || o === null) {
             return o;
         }
         
