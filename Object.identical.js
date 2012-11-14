d86d7201c518ed850dd8670429e4f58c83e43d4d
Chris <accounts@prettycode.org>
2012-11-14 08:52:09 -0800

    Removing "license" requirement. Putting restrictions on code re-use is lame.

--- Object.identical.js
+++ Object.identical.js
@@ -2,10 +2,6 @@
     Original script title: "Object.identical.js"; version 1.12
     Copyright (c) 2011, Chris O'Brien, prettycode.org
     http://github.com/prettycode/Object.identical.js
-
-    Permission is hereby granted for unrestricted use, modification, and redistribution of this
-    script, only under the condition that this code comment is kept wholly complete, appearing
-    directly above the script's code body, in all original or modified non-minified representations
 */
 
 Object.identical = function (a, b, sortArrays) {
